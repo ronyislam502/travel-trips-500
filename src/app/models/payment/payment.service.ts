cd71dbd81f4916eeca79e0f71d193ef97c6a83b0
ronyislam502 <ronyislam502@gmail.com>
2024-11-05 00:42:43 +0600

    payment

---
+++ src/app/models/payment/payment.service.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import User from "../user/user.model";
+import Payment from "./payment.model";
+
+export const createPayment = async (
+  amount: number,
+  transactionId: string,
+  userId: string
+) => {
+  await Payment.create({
+    amount: amount,
+    transactionId,
+    status: "Paid",
+  });
+
+  await User.findByIdAndUpdate(userId, {
+    isPremium: true,
+  });
+
+  const filePath = join(__dirname, "../../templates/success.html");
+  let file = readFileSync(filePath, "utf-8");
+  file = file.replace("{{link}}", "/");
+
+  return file;
+};
+export const failedPayment = async () => {
+  const filePath = join(__dirname, "../../templates/error.html");
+  let file = readFileSync(filePath, "utf-8");
+  file = file.replace("{{link}}", "/");
+  return file;
+};
+
+export const paymentService = {
+  createPayment,
+  failedPayment,
+};
